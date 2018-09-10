b54b756a19d5fe6ee3af5e43ce07b137f363cd3e
Mark Mangahas <mo.mangahas@yahoo.com>
2018-09-10 14:40:52 -0700

    fixing draft

--- WordCounter.Tests/Model.Tests/RepeatCounterTest.cs
+++ WordCounter.Tests/Model.Tests/RepeatCounterTest.cs
@@ -21,36 +21,6 @@ namespace WordCounter.Tests
       //Assert
       Assert.AreEqual(input, result);
     }
-    [TestMethod]
-    public void SetInput_GetInput_String()
-    {
-      // Arrange
-      string input = "Macbook";
-      RepeatCounter newRepeatCounter = new RepeatCounter(input);
-
-      // Act
-      string secondDescription = "Microsoft";
-      newRepeatCounter.SetInput(secondDescription);
-      string result = newRepeatCounter.GetInput();
-
-      // Assert
-      Assert.AreEqual(secondDescription, result);
-    }
-    [TestMethod]
-    public void CountTheWords()
-    {
-      // Arrange
-      string count = "numbers";
-      string countChar[] = new countChar(count);
-
-       // Act
-       string secoundCount = "numbersxtwo"
-       newcountChar.SetInput(secondCount);
-       string result = newcountChar.GetInput();
-
-       // Assert
-      Assert.AreEqual(secondCount, result)
-    }
     public void Dispose()
     {
       RepeatCounter.ClearAll();
