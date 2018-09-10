c14901cb28210131e22c863c069fbbb3d1b6928e
Mark Mangahas <mo.mangahas@yahoo.com>
2018-09-10 15:57:04 -0700

    finishing main cs file

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -1,40 +1,64 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    private string _text;
     private string _input;
-    private int _count = 0;
 
-    public int CountTheWords(int count)
-    {
-     _count = count;
-     {
-      for(int i = 0; i < input.Length + 1; i ++)
-        _count += (_input == inputs[i]) ? 1 : 0;
-  }
- }
-    public void SetPrivateStrings(string input, int count)
+    public void SetPrivateInput(string input)
     {
       _input = input;
-      _count = count;
     }
-    public void GetPrivateStrings()
+
+    public string GetPrivateInput()
     {
       return _input;
-      return _count;
+
     }
 
-  public class program
-  {
-    public static void Main ()
-  {
-    Console.WriteLine("Welcome to WordCounter Program");
-    Console.WriteLine("Please type in a word");
-    Console.ReadLine() = input;
+    public void SetPrivateText(string text)
+    {
+      _text = text;
+    }
 
+    public string GetPrivateText()
+    {
+      return _text;
 
- }
+    }
+
+    public char[] SplittheInput()
+    {
+    char[] splitInput = _input.Split(' ');
+    return splitInput;
+    }
+
+    public int Count()
+    {
+      int counts = 0;
+      foreach(string text in SplitSentence())
+     {
+    if (words.Equals(_text))
+      {
+       counts ++;
+      }
+       return _text;
+      }
+    }
+  }
+
+  public class Program
+  {
+      public static void Main()
+    {
+      Console.WriteLine("Welcome to WordCounter Program");
+      Console.WriteLine("Please type in a word");
+      // string wordInput = Console.ReadLine();
+      // RepeatCounter wordInput = new RepeatCounter();
+
+    }
+  }
 }
